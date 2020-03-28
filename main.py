2531f385f087370635a4f85d2fb75b12338c729a
WLO <loukilwassim@gmail.com>
2020-03-28 17:14:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
+import random
+
+def getname():
+    while True:
+        name=(str(input("Veuillez enter votre nom \n")))
+        if (len(name) < 3):
+            print("Minimaum 3 caractere \n")
+        else:
+            break
+    return(name)
+
+
 def getage():
     while True:
         try:
-            age=(int(input("Veuillez saisir votre age: ")))
+            age=(int(input("Veuillez saisir votre age: \n")))
             if (age <0 or age >= 120):
-                print("Veuillez saisir un age compris entre 1 et 120 !")
+                print("Veuillez saisir un age compris entre 1 et 120 !\n")
             else:
                break
         except ValueError:
-            print("Veuillez saisir un entien SVP !")
-    print(age)
+            print("Veuillez saisir un entien SVP !\n")
     return (age)
-
-
-
-getage()
